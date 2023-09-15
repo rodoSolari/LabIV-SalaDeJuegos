c5203391665b6e66f08d2ef781b074a697e073c7
rodoSolari <rodosolari033@gmail.com>
2023-09-14 22:37:51 -0300

    sprint 1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './componentes/home/home.component';
+import { QuienSoyComponent } from './componentes/quien-soy/quien-soy.component';
+import { LoginComponent } from './componentes/login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:HomeComponent},
+  {path:'home',component:HomeComponent},
+  {path:'quien-soy',component:QuienSoyComponent},
+  {path:'login',component:LoginComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './componentes/login/login.component';
+import { HomeComponent } from './componentes/home/home.component';
+import { QuienSoyComponent } from './componentes/quien-soy/quien-soy.component';
+import { FormsModule } from '@angular/forms';
+import { NavbarComponent } from './componentes/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HomeComponent,
+    QuienSoyComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
